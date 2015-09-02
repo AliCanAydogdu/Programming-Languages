8c3700244870d00381436714b1e8d8a740157f19
Enes Kemal Ergin <eneskemalergin@gmail.com>
2015-09-02 09:41:12 -0500

    Recursive and Iterative QuickSort

---
+++ Class_Assignments/C2Q2_Recursive_QuickSort.c
@@ -0,0 +1,62 @@
+/* To understand the value of recursion in a programming language, write a
+ * program that implements quicksort, first using recursion and then without
+ * recursion
+ */
+
+ /* Author: Enes Kemal Ergin
+  * Assignment: Assignment 1P - Extra 1 - recursive Quick Sort
+  * Class : Programming Languages
+  * Date : 09/01/15
+  */
+
+#include <stdio.h>
+
+void quicksort(int [10],int,int);
+
+int main(){
+  int x[20],size,i;
+
+  printf("Enter size of the array: ");
+  scanf("%d",&size);
+
+  printf("Enter %d elements: ",size);
+  for(i=0;i<size;i++)
+    scanf("%d",&x[i]);
+
+  quicksort(x,0,size-1);
+
+  printf("Sorted elements: ");
+  for(i=0;i<size;i++)
+    printf(" %d",x[i]);
+
+  return 0;
+}
+
+void quicksort(int x[10],int first,int last){
+    int pivot,j,temp,i;
+
+     if(first<last){
+         pivot=first;
+         i=first;
+         j=last;
+
+         while(i<j){
+             while(x[i]<=x[pivot]&&i<last)
+                 i++;
+             while(x[j]>x[pivot])
+                 j--;
+             if(i<j){
+                 temp=x[i];
+                  x[i]=x[j];
+                  x[j]=temp;
+             }
+         }
+
+         temp=x[pivot];
+         x[pivot]=x[j];
+         x[j]=temp;
+         quicksort(x,first,j-1);
+         quicksort(x,j+1,last);
+
+    }
+}
