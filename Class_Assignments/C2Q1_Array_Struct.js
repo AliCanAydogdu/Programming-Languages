69cb5164824f1daf1c2899c3d4f733b3da6f1876
Enes Kemal Ergin <eneskemalergin@gmail.com>
2015-09-02 19:57:54 -0500

    Array Struct question in javaScript

---
+++ Class_Assignments/C2Q1_Array_Struct.js
@@ -0,0 +1,41 @@
+/* To understand the value of records in a programming language,
+ * write a small program in a C-based language that uses
+ * an array of structs that store student information, including name,
+ * age, GPA as a float, and grade level as a string
+ * (e.g., “freshmen,” etc.).
+ */
+
+/* Author: Enes Kemal Ergin
+ * Assignment: Assignment 1P
+ * Class : Programming Languages
+ * Date : 09/02/15
+ */
+
+ // This is the first way of defining objects
+ var student1 = {
+   name: "Enes",
+   age : 21,
+   gpa : 3.68,
+   gradeLevel: "Senior"
+ }
+
+// Prints name of the student1
+console.log(student1.name)
+
+// Prints gpa of the student1
+console.log(student1.age)
+
+// Prints the whole object
+
+console.log(student1)
+
+// This is another way of defining objects
+student2.name = "Joe";
+student2.age = 23;
+student2.gpa = 1.2;
+student2.gpa = "Junior";
+
+console.log(student2);
+
+
+// There is third way but eehh,
