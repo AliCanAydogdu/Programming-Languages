224be5cef442806f402458ef2ddc6cddd08d187a
Enes Kemal Ergin <eneskemalergin@gmail.com>
2015-09-02 19:38:02 -0500

    Array Struct question in Python

---
+++ Class_Assignments/C2Q1_Array_Struct.py
@@ -0,0 +1,12 @@
+# To understand the value of records in a programming language,
+# write a small program in a C-based language that uses
+# an array of structs that store student information, including name,
+# age, GPA as a float, and grade level as a string
+# (e.g., “freshmen,” etc.).
+
+# Author: Enes Kemal Ergin
+# Assignment: Assignment 1P
+# Class : Programming Languages
+# Date : 09/01/15
+
+Student = {name: "Enes", age: 21, gpa: 4.68, gradeLevel: "Senior"}
