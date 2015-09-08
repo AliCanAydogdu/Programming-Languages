64a40c8181216a9cefbd0d8aa50cb0145146fc91
Enes Kemal Ergin <eneskemalergin@gmail.com>
2015-09-08 11:34:56 -0500

    Header added in R version of the quick sort algorithm

--- Class_Assignments/Extra Assignments/C2Q2_Recursive_QuickSort.R
+++ Class_Assignments/Extra Assignments/C2Q2_Recursive_QuickSort.R
@@ -1,17 +1,27 @@
+# To understand the value of recursion in a programming language, write a
+# program that implements quicksort, first using recursion and then without
+# recursion
+
+
+ # Author: Enes Kemal Ergin
+ # Assignment: Assignment 1P - Extra 1 - Recursive Quick Sort
+ # Class : Programming Languages
+ # Date : 09/08/15
+
 # I used vectors like lists/arrays here
 quicksort <- function(vec) {
-  
+
   if(length(vec) > 1) {
-    
+
     pivot <- vec[1]
-    
+
     low <- quicksort(vec[vec < pivot])
     mid <- vec[vec == pivot]
     high <- quicksort(vec[vec > pivot])
-    
-    c(low, mid, high)  
+
+    c(low, mid, high)
   }
-  
+
   else vec
 }
 
