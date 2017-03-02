1f430fce4b8e6d84de2ff7053ff48a35c59e55e2
Ali Can Aydogdu <alicantx@gmail.com>
2017-03-02 15:47:06 -0600

    Update C2Q1_Array_Struct.c

--- Class_Assignments/Assignment1/C2Q1_Array_Struct.c
+++ Class_Assignments/Assignment1/C2Q1_Array_Struct.c
@@ -1,16 +1,3 @@
-/* To understand the value of records in a programming language,
- * write a small program in a C-based language that uses
- * an array of structs that store student information, including name,
- * age, GPA as a float, and grade level as a string
- * (e.g., “freshmen,” etc.).
- */
-
-/* Author: Enes Kemal Ergin
- * Assignment: Assignment 1P
- * Class : Programming Languages
- * Date : 09/01/15
- */
-
  #include <stdio.h>
  #include <string.h>
 
